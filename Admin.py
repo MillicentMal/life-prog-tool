6c3fadc64c4f5d4b4441ae635c9cddff1ca86cbf
Joshh99 <joshuawisdom92@gmail.com>
2024-08-20 01:12:05 +0200

    Admin.py: adds the inherited attributes

--- Admin.py
+++ Admin.py
@@ -9,10 +9,8 @@ class Admin:
     """
 
     def __init__(self, email, password, first_name, last_name):
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
+        super().__init__(email, password, first_name, last_name)
+        
 
     @property
     def email(self):
